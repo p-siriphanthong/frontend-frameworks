ad5492517e148578df9a4b83de9acaa1a4729f53
Punn Siriphanthong <p.siriphanthong@gmail.com>
2020-11-10 01:47:30 +0700

    refactor: render react todo app by getElementById

--- src/react/index.js
+++ src/react/index.js
@@ -2,4 +2,4 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import TodoList from './TodoList'
 
-ReactDOM.render(React.createElement(TodoList), document.querySelector('#react'))
+ReactDOM.render(React.createElement(TodoList), document.getElementById('react'))
